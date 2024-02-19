b206714f076b85593d2887be135b31a8dba92227
testmysiteon1@gmail.com <testmysiteon1@gmail.com>
2024-02-19 23:28:00 +0530

    added project

---
+++ FoodDelivery/FoodDelivery/Areas/Restaurant/Controllers/DashBoardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+namespace FoodDelivery.Areas.Restaurant.Controllers
+{
+    [Area("restaurant")]
+    public class DashboardController : BaseController
+    {
+        public IActionResult Index(bool IsRestricted = false)
+        {
+            if (IsRestricted)
+            {
+                ViewBag.Message = "You are now allowed to access it!";
+                ViewBag.MessageType = "danger";
+            }
+           
+            return View();
+        }
+
+       
+    }
+}
