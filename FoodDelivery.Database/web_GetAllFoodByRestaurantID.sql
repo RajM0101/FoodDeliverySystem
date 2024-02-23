6d307111809c8cf88c31a3957a495d745abaaa49
testmysiteon1@gmail.com <testmysiteon1@gmail.com>
2024-02-23 22:35:03 +0530

    Add store procedure for get restaurant and food

---
+++ FoodDelivery.Database/web_GetAllFoodByRestaurantID.sql
@@ -0,0 +1,23 @@
+﻿
+CREATE PROCEDURE [dbo].[web_GetAllFoodByRestaurantID]
+@RestaurantID INT=0
+AS 
+BEGIN  
+
+	 SELECT 
+		 RestaurantID
+		 ,FoodID
+		 ,FoodName
+		 ,Price
+		 ,ImageName
+		 ,Ingredient
+		 ,DiscountInPercentage
+		 ,IsBestSeller
+		 ,IsVegetarian 
+	 FROM Food 
+	 WHERE RestaurantID=@RestaurantID AND IsAvailable=1 AND ISNULL(IsDeleted,0)=0
+	 ORDER BY DisplayOrder,IsBestSeller
+
+ END
+
+GO
---
+++ FoodDelivery.Database/web_GetAllRestaurant.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [dbo].[web_GetAllRestaurant]
+AS 
+BEGIN  
+
+	Select 
+		RestaurantID
+		,RestaurantName
+		,ShopPlotNumber +Floor +BuildingName as Address
+		,ZipCode
+		,ImageName
+	 from Restaurant
+	where IsActive=1 AND Isnull(IsDelete,0)=0
+
+ end
+
+GO
